00a667dc8f848db3e3ecca7d526043a5eddcce69
Viktor <viktoryanenko11@gmail.com>
2025-03-20 13:13:45 +0200

    complete 2nd task, add styles to start page

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('.feedback-form');
-
 const LS_KEY = 'feedback-form-state';
 
 const formData = {
@@ -7,6 +6,34 @@ const formData = {
   message: '',
 };
 
-function saveToLocalStorage() {
+const savedFormData = localStorage.getItem(LS_KEY);
+if (savedFormData) {
+  const parsedFormData = JSON.parse(savedFormData);
+  formData.email = parsedFormData.email;
+  formData.message = parsedFormData.message;
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+function addToLocalStorage(event) {
+  formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(LS_KEY, JSON.stringify(formData));
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(LS_KEY);
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+}
+
+form.addEventListener('input', addToLocalStorage);
+
+form.addEventListener('submit', handleSubmit);
